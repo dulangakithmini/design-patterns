8bd1393aad5266ca8d84f6d7ca49d2a205737baf
Dulanga Kithmini <dulangakithmini@gmail.com>
2021-05-13 01:26:09 +0530

    complete Observer Design pattern

--- src/Observer/WeatherStation.java
+++ src/Observer/WeatherStation.java
@@ -4,8 +4,16 @@ public class WeatherStation {
     public static void main(String[] args) {
         WeatherData weatherData = new WeatherData();
         CurrentConditionsDisplay currentConditionsDisplay = new CurrentConditionsDisplay("Current Condition");
+        StatisticsDisplay statisticsDisplay = new StatisticsDisplay("Statistics");
 
         weatherData.registerObserver(currentConditionsDisplay);
+        weatherData.registerObserver(statisticsDisplay);
+
         weatherData.setMeasurements(100, 20, 25);
+
+        weatherData.removeObserver(currentConditionsDisplay);
+
+        System.out.println("After unsubscribing Current Conditions Display");
+        weatherData.setMeasurements(120, 30, 35);
     }
 }

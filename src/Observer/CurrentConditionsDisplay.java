6de0db0001839fb18cb6a63b24ed9df07073f7bc
Dulanga Kithmini <dulangakithmini@gmail.com>
2021-05-13 00:44:53 +0530

    Create CurrentConditionsDisplay implementing Observer interface

---
+++ src/Observer/CurrentConditionsDisplay.java
@@ -0,0 +1,14 @@
+package Observer;
+
+public class CurrentConditionsDisplay implements Observer {
+    private float temperature;
+    private float humidity;
+    private float pressure;
+
+    @Override
+    public void update(float temperature, float humidity, float pressure) {
+        this.temperature = temperature;
+        this.humidity = humidity;
+        this.pressure = pressure;
+    }
+}

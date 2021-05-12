40a558dc97b8044059af2b05c2d313aa7129cfaf
Dulanga Kithmini <dulangakithmini@gmail.com>
2021-05-12 17:12:38 +0530

    Implement double-checked locking

---
+++ src/DoubleCheckedLocking.java
@@ -0,0 +1,17 @@
+public class DoubleCheckedLocking {
+    private volatile static DoubleCheckedLocking instance;
+
+    private DoubleCheckedLocking() {
+    }
+
+    public static DoubleCheckedLocking getInstance() {
+        if (instance == null) {
+            synchronized (DoubleCheckedLocking.class) {
+                if (instance == null) {
+                    instance = new DoubleCheckedLocking();
+                }
+            }
+        }
+        return instance;
+    }
+}

ed0f3ca92d355bfcedbf3271d36e4a381dbe7edd
Dulanga Kithmini <dulangakithmini@gmail.com>
2021-05-12 17:05:34 +0530

    Implement Eager initialization

---
+++ src/EagerInitialization.java
@@ -0,0 +1,10 @@
+public class EagerInitialization {
+    public static EagerInitialization instance = new EagerInitialization();
+
+    private EagerInitialization() {
+    }
+
+    public static EagerInitialization getInstance() {
+        return instance;
+    }
+}

9b5a9c433b08c58c0a6b87b008a1dfc602f2b692
Dulanga Kithmini <dulangakithmini@gmail.com>
2021-05-13 00:26:02 +0530

    Create WeatherData class implementing Subject interface

---
+++ src/Observer/WeatherData.java
@@ -0,0 +1,31 @@
+package Observer;
+
+import java.util.ArrayList;
+
+public class WeatherData implements Subject {
+    private ArrayList<Observer> observers;
+    private float temperature;
+    private float humidity;
+    private float pressure;
+
+    public WeatherData() {
+        observers = new ArrayList<Observer>();
+    }
+
+    @Override
+    public void registerObserver(Observer o) {
+        observers.add(o);
+    }
+
+    @Override
+    public void removeObserver(Observer o) {
+        observers.remove(o);
+    }
+
+    @Override
+    public void notifyObservers() {
+        for (Observer observer : observers) {
+            observer.update(temperature, humidity, pressure);
+        }
+    }
+}

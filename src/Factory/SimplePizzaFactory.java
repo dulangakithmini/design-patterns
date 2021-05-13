4d7e13d2c07659cb30f9f2e8cf474abbc807a8c3
Dulanga Kithmini <dulangakithmini@gmail.com>
2021-05-13 21:23:54 +0530

    Create SimplePizzaFactory

---
+++ src/Factory/SimplePizzaFactory.java
@@ -0,0 +1,19 @@
+package Factory;
+
+public class SimplePizzaFactory {
+
+    public Pizza cretePizza(String type) {
+        Pizza pizza = null;
+
+        if (type == "Cheese") {
+            pizza = new CheesePizza();
+        } else if (type == "Veggie") {
+            pizza = new VeggiePizza();
+        } else if (type == "Clam") {
+            pizza = new ClamPizza();
+        } else if (type == "Pepperoni") {
+            pizza = new PepperoniPizza();
+        }
+        return pizza;
+    }
+}

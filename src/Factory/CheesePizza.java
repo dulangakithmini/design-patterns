d18172a5ea07d6961ce8f94aab431ad0d071ac39
Dulanga Kithmini <dulangakithmini@gmail.com>
2021-05-13 21:12:07 +0530

    Create CheesePizza class extending Pizza abstract class

---
+++ src/Factory/CheesePizza.java
@@ -0,0 +1,24 @@
+package Factory;
+
+public class CheesePizza extends Pizza {
+
+    @Override
+    void prepare() {
+        System.out.println("Add cheese");
+    }
+
+    @Override
+    void bake() {
+        System.out.println("Bake Cheese pizza");
+    }
+
+    @Override
+    void cut() {
+        System.out.println("Cut the Cheese pizza");
+    }
+
+    @Override
+    void box() {
+        System.out.println("Box the Cheese pizza");
+    }
+}

b24ee4f8cca44a49a29f50b7185fedc227d80d51
Dulanga Kithmini <dulangakithmini@gmail.com>
2021-05-13 21:17:15 +0530

    Create PepperoniPizza class extending Pizza abstract class

---
+++ src/Factory/PepperoniPizza.java
@@ -0,0 +1,24 @@
+package Factory;
+
+public class PepperoniPizza extends Pizza {
+
+    @Override
+    void prepare() {
+        System.out.println("Add pepperoni");
+    }
+
+    @Override
+    void bake() {
+        System.out.println("Bake Pepperoni pizza");
+    }
+
+    @Override
+    void cut() {
+        System.out.println("Cut the Pepperoni pizza");
+    }
+
+    @Override
+    void box() {
+        System.out.println("Box the Pepperoni pizza");
+    }
+}

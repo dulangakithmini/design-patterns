373dde3d3206a183177a8b49be4d003021b4e540
Dulanga Kithmini <dulangakithmini@gmail.com>
2021-05-15 01:18:32 +0530

    Create Main class

---
+++ src/Composite/Main.java
@@ -0,0 +1,25 @@
+package Composite;
+
+public class Main {
+    public static void main(String[] args) {
+        MenuComponent pancakeHouseMenu = new Menu("Pancake House Menu", "Breakfast");
+        MenuComponent dinerMenu = new Menu("Diner Menu", "Lunch");
+        MenuComponent cafeMenu = new Menu("Cafe Menu", "Dinner");
+        MenuComponent dessertMenu = new Menu("Dessert Menu", "Dessert");
+
+        MenuComponent allMenus = new Menu("All Menus", "All menus combined");
+
+        allMenus.add(pancakeHouseMenu);
+        allMenus.add(dinerMenu);
+        allMenus.add(cafeMenu);
+
+        dinerMenu.add(dessertMenu);
+
+        pancakeHouseMenu.add(new MenuItem("French Crêpes", "Made from flour, milk and eggs, these are closest counterparts to English pancakes, and can be served sweet or savoury.", 100, false));
+        pancakeHouseMenu.add(new MenuItem("Scotch Pancakes", "Made with flour, eggs, sugar, milk, salt and cream of tartar.", 120, false));
+        pancakeHouseMenu.add(new MenuItem("Spaghetti", "With garlic and cheese", 200, true));
+
+        dinerMenu.add(new MenuItem("Burger", "With extra chese and meat", 700, false));
+//        dinerMenu.add(new MenuItem("Sandwitch", "With extra cheese" +));
+    }
+}

efa31ffb97fe3684f4c05977f2612ba3171ecd41
Dulanga Kithmini <dulangakithmini@gmail.com>
2021-05-16 03:24:45 +0530

    Give HomeTheaterFacade access to the components of the subsystem through composition.

--- src/Facade/HomeTheaterFacade.java
+++ src/Facade/HomeTheaterFacade.java
@@ -5,12 +5,18 @@ public class HomeTheaterFacade {
     PopcornPopper popcornPopper;
     DvdPlayer dvd;
     Screen screen;
+    Amplifier amplifier;
+    Projector projector;
+    TheaterLights theaterLights;
 
     //    The facade is passed a reference to each component of the subsystem in its constructor. The facade then assigns each to the corresponding variable.
-    public HomeTheaterFacade(PopcornPopper popcornPopper, DvdPlayer dvd, Screen screen) {
+    public HomeTheaterFacade(PopcornPopper popcornPopper, DvdPlayer dvd, Screen screen, Amplifier amplifier, Projector projector, TheaterLights theaterLights) {
         this.popcornPopper = popcornPopper;
         this.dvd = dvd;
         this.screen = screen;
+        this.amplifier = amplifier;
+        this.projector = projector;
+        this.theaterLights = theaterLights;
     }
 
     //    bring the components of the subsystem together into a unified interface

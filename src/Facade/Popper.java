0eb7ec7114f6a14ffd4301fa42b7ce4dd48277ca
Dulanga Kithmini <dulangakithmini@gmail.com>
2021-05-16 03:05:54 +0530

    Change pop() in Popper class

--- src/Facade/Popper.java
+++ src/Facade/Popper.java
@@ -3,11 +3,10 @@ package Facade;
 public class Popper {
     public void on() {
         System.out.println("Popper is on");
-        pop(10);
     }
 
-    public void pop(int min) {
-        System.out.println("Pops once every " + min + " minutes");
+    public void pop() {
+        System.out.println("Popper pops");
     }
 
     public void off() {

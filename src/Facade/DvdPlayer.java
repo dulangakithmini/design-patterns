d40e3683fd3485d3c3c148e66dede304fe552478
Dulanga Kithmini <dulangakithmini@gmail.com>
2021-05-16 02:59:56 +0530

    Create DvdPlayer class

---
+++ src/Facade/DvdPlayer.java
@@ -0,0 +1,11 @@
+package Facade;
+
+public class DvdPlayer {
+    public void on() {
+        System.out.println("On the DVD");
+    }
+
+    public void off() {
+        System.out.println("Off the DVD");
+    }
+}

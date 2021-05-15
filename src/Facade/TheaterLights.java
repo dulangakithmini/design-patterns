445ed491c560d78733d9b8543402ae14f29b6d92
Dulanga Kithmini <dulangakithmini@gmail.com>
2021-05-16 03:21:35 +0530

    Create TheaterLights class

---
+++ src/Facade/TheaterLights.java
@@ -0,0 +1,11 @@
+package Facade;
+
+public class TheaterLights {
+    public void on() {
+        System.out.println("Lights are on");
+    }
+
+    public void dim() {
+        System.out.println("Lights are dim");
+    }
+}

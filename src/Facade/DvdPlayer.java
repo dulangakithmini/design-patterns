e2dfdc3632e1b7d9b567e1168a2c0b8af01e3e5e
Dulanga Kithmini <dulangakithmini@gmail.com>
2021-05-16 03:16:50 +0530

    Complete DvdPlayer class

--- src/Facade/DvdPlayer.java
+++ src/Facade/DvdPlayer.java
@@ -5,7 +5,19 @@ public class DvdPlayer {
         System.out.println("On the DVD");
     }
 
+    public void play(String movie) {
+        System.out.println(movie + " plays");
+    }
+
+    public void stop() {
+        System.out.println("Stop the DVD player");
+    }
+
+    public void eject() {
+        System.out.println("DVD ejected");
+    }
+
     public void off() {
-        System.out.println("Off the DVD");
+        System.out.println("Off the DVD player");
     }
 }

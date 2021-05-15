be3e2462e963b20b3ccf7e9c91653ac1653e72e7
Dulanga Kithmini <dulangakithmini@gmail.com>
2021-05-16 03:19:28 +0530

    Create Projector class

---
+++ src/Facade/Projector.java
@@ -0,0 +1,15 @@
+package Facade;
+
+public class Projector {
+    public void on() {
+        System.out.println("Projector is on");
+    }
+
+    public void wideScreenKMode() {
+        System.out.println("Projector is in wide screen mode");
+    }
+
+    public void off() {
+        System.out.println("Projector is off");
+    }
+}

91ed880eaebff95e8fc00007f6dcb8456e7593d8
Dulanga Kithmini <dulangakithmini@gmail.com>
2021-05-16 03:13:41 +0530

    Create Amplifier class

---
+++ src/Facade/Amplifier.java
@@ -0,0 +1,15 @@
+package Facade;
+
+public class Amplifier {
+    public void on() {
+        System.out.println("Amplifier is on");
+    }
+
+    public void setSurroundingSound() {
+        System.out.println("Surrounding sound is set");
+    }
+
+    public void setVolume(int volume) {
+        System.out.println("Volume is set to " + volume);
+    }
+}
